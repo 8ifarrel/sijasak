8d67fb1b6113b9976051779b44afc1588bfc597b
8ifarrel <farrelsirah@gmail.com>
2025-05-19 21:20:26 +0800

    Menginisialisasikan ArcGIS API

--- resources/views/guest/layouts/beranda.blade.php
+++ resources/views/guest/layouts/beranda.blade.php
@@ -2,45 +2,101 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-  <meta name="description" content="{{ $meta_description }}" />
+    <meta name="description" content="{{ $meta_description }}" />
 
-  <title>
-    {{ $page_title }} | {{ config('app.name') }} ({{ config('app.name_short') }}) {{ config('app.location') }}
-  </title>
+    <title>
+        {{ $page_title }} | {{ config('app.name') }} ({{ config('app.name_short') }}) {{ config('app.location') }}
+    </title>
 
-  <link rel="icon" type="image/x-icon" href="" />
+    <link rel="icon" type="image/x-icon" href="" />
 
-  {{-- Fontawesome --}}
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Fontawesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  {{-- AOS --}}
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    {{-- AOS --}}
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
-  {{-- Tailwind CSS & Preline UI --}}
-  @vite('resources/css/app.css')
-  @vite('resources/js/app.js')
+    {{-- ArcGIS --}}
+    <link rel="stylesheet" href="https://js.arcgis.com/4.28/esri/themes/light/main.css" />
+
+    {{-- Tailwind CSS & Preline UI --}}
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
 </head>
 
 <body>
-  @include('guest.components.navbar')
+    @include('guest.components.navbar')
 
-  @yield('slot')
+    @yield('slot')
 
-  @include('guest.components.footer')
+    @include('guest.components.footer')
 
-  {{-- AOS --}}
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js">
-    AOS.init({
-      duration: 800,
-    });
-  </script>
+    {{-- AOS --}}
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js">
+        AOS.init({
+            duration: 800,
+        });
+    </script>
+
+    {{-- ArcGIS --}}
+    <script src="https://js.arcgis.com/4.28/"></script>
+    <script>
+        require([
+            "esri/Map",
+            "esri/views/MapView",
+            "esri/widgets/Locate",
+            "esri/widgets/ScaleBar",
+            "esri/widgets/Compass"
+        ], function(Map, MapView, Locate, ScaleBar, Compass) {
+            var map = new Map({
+                basemap: "hybrid"
+            });
+
+            var view = new MapView({
+                container: "arcgisMap",
+                map: map,
+                center: [117.1466, -0.5022], // Koordinat Kota Samarinda
+                zoom: 12
+            });
+
+            view.ui.move("zoom", "bottom-right");
+
+            var locateWidget = new Locate({
+                view: view,
+                geolocationOptions: {
+                    enableHighAccuracy: true,
+                    maximumAge: 0,
+                    timeout: 15000
+                }
+            });
+            view.ui.add(locateWidget, {
+                position: "bottom-right"
+            });
+
+            var compass = new Compass({
+                view: view
+            });
+            view.ui.add(compass, {
+                position: "bottom-left"
+            });
+
+            var scaleBar = new ScaleBar({
+                view: view,
+                unit: "metric"
+            });
+            view.ui.add(scaleBar, {
+                position: "bottom-left"
+            });
+        });
+    </script>
 </body>
 
 </html>
--- resources/views/guest/pages/beranda/index.blade.php
+++ resources/views/guest/pages/beranda/index.blade.php
@@ -1,5 +1,5 @@
 @extends('guest.layouts.beranda')
 
 @section('slot')
-<h1>ayam</h1>
+<div id="arcgisMap" class="fixed top-0 left-0 w-screen h-screen z-0"></div>
 @endsection
